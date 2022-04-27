460f36a7fc89d64a319fe83bd8aef570a92fcd46
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-27 16:14:01 -0700

    finished traffic function, all working properly

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Vehicle from './Vehicle.js';
 
 export default function VehicleList({ vehicles }) {
   return (
     <div className="traffic">
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
+      {
+        vehicles.map(vehicle => 
+          <Vehicle 
+            vehicles={vehicle}
+            key={vehicle}
+            vehicle={vehicle}/>)
+      }
       {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
     </div>
   );
