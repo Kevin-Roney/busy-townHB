0accd38a48d6233be4fb88e3fdac31f0ef6bd7d9
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-27 17:20:27 -0700

    fixed key error by adding index value to .map and changed test parameters in App.test.js

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -7,11 +7,11 @@ export default function VehicleList({ vehicles }) {
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
       {
-        vehicles.map(vehicle => 
+        vehicles.map((vehicle, index) => 
           <Vehicle 
             vehicles={vehicle}
-            key={vehicle}
-            vehicle={vehicle}/>)
+            key={vehicle + index}
+          />)
       }
       {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
     </div>
